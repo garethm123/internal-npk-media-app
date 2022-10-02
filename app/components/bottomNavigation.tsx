fb7f1bf576b36cf0780a5969df9b46a25213b7d7
garethm123 <garethmay@hotmail.co.uk>
2022-10-02 11:10:25 +0100

    Updating bottom navigation style

--- app/components/bottomNavigation.tsx
+++ app/components/bottomNavigation.tsx
@@ -4,8 +4,9 @@ import {
 } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/Home";
 import SettingsScreen from "../screens/Settings";
-import { View } from "react-native";
 import AddEventScreen from "../screens/AddEvent";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { View } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
@@ -14,12 +15,46 @@ export default function BottomNavigation() {
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
+        tabBarStyle: {
+          height: 60,
+          paddingHorizontal: 5,
+          paddingTop: 0,
+          backgroundColor: '#FD397A',
+          position: 'absolute',
+          borderTopWidth: 0,
+        },
+        // tabBarActiveTintColor: 'black',
+        // tabBarInactiveTintColor: 'white',
+        tabBarShowLabel: false
       }}
     >
-      <Tab.Screen name='Home' component={HomeScreen} />
-      <Tab.Screen name='AddEvent' component={AddEventScreen} />
-      {/* <Tab.Screen name='NotifyAll' component={}/> */}
-      <Tab.Screen name='Settings' component={SettingsScreen} />
+      <Tab.Screen name='Home' component={HomeScreen} options={{
+        tabBarIcon: ({size,focused,color}) => {
+          return (
+            <View>
+              <Icon name='home' size={30} color={focused ? "black" : 'white'} />
+            </View>
+          );
+        },
+      }}/>
+      <Tab.Screen name='AddEvent' component={AddEventScreen}  options={{
+        tabBarIcon: ({size,focused,color}) => {
+          return (
+            <View>
+              <Icon name='plus' size={30} color={focused ? "black" : 'white'} />
+            </View>
+          );
+        },
+      }}/>
+      <Tab.Screen name='Settings' component={SettingsScreen}  options={{
+        tabBarIcon: ({size,focused,color}) => {
+          return (
+            <View>
+              <Icon name='cog' size={30} color={focused ? "black" : 'white'} />
+            </View>
+          );
+        },
+      }}/>
     </Tab.Navigator>
   );
 }
--- app/styles/globalStyles.tsx
+++ app/styles/globalStyles.tsx
@@ -10,6 +10,10 @@ export const GlobalStyles = StyleSheet.create({
     fontFamily: 'montserrat-regular',
     fontSize: 18
   },
+  yellowBackground: {
+    backgroundColor: '#fba220',
+    minHeight: '100%',
+  },
   fullWidthPaddedContainer: {
     width: '100%',
     padding: 24,
@@ -17,13 +21,15 @@ export const GlobalStyles = StyleSheet.create({
   modalWrapper: {
     width: '100%',
     height: '100%',
-    marginVertical: 90,
+    // marginVertical: 90,
+    display: 'flex',
+    flex: 1,
     background: '#96c6d9'
   },
   ceteredContent: {
     height: '100%',
     width: '100%',
-    padding: 24,
+    paddingHorizontal: 24,
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center'
